d2f5efbd616ff1b1c465c7ca6d874824eacfc8a0
Rachel Rost <rachel.v.rost@gmail.com>
2016-10-05 15:54:30 -0500

    Transferred HTML into home.php

--- views/home.php
+++ views/home.php
@@ -1,27 +1,159 @@
-<div class="container">
-
-    <section id="welcome">
-
-        <div class="row">
-
-            <div class="col-xs-12">
-
-                <h1 class="text-center">Welcome To Adlister</h1>
-
+    <div class="home-carousel">
+            <div class="dark-mask"></div>
+            <div class="container">
+                <div class="homepage owl-carousel">
+                    <div class="item">
+                        <div class="row">
+                            <div class="col-sm-5 right">
+                                <p>
+                                    <img src="img/gaglister-logo-white.png" alt="">
+                                </p>
+                                <h1>One Stop - <br>Gag Shop.</h1>
+                                <p>Laughs. Gifts. Fun.
+                                </p>
+                            </div>
+                            <div class="col-sm-7">
+                                <img class="img-responsive" src="img/template-easy-customize.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                    
+                </div>
+                <!-- /.project owl-slider -->
             </div>
-
         </div>
+        <!-- *** HOMEPAGE CAROUSEL END *** -->
+        </section>
+        <section class="bar background-pentagon no-mb">
+            <div class="container">
+                <div class="row showcase">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="item">
+                            <div class="icon"><i class="fa fa-cart-plus"></i>
+                            </div>
+                            <h4><span class="counter">580</span><br>
 
-    </section>
+        Items Listed</h4>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="item">
+                            <div class="icon"><i class="fa fa-users"></i>
+                            </div>
+                            <h4><span class="counter">496</span><br>
 
-    <section id="features">
+        Registered Users</h4>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="item">
+                            <div class="icon"><i class="fa fa-money"></i>
+                            </div>
+                            <h4><span class="counter">5621</span><br>
 
-        <div class="row">
+        Items Sold</h4>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="item">
+                            <div class="icon"><i class="fa fa-smile-o"></i>
+                            </div>
+                            <h4><span class="counter">1,000,000</span><br> 
 
-            <h3 class="section-title">Featured Items</h3>
-            <!-- Placeholder for featured items.-->
+        Laughs</h4>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container -->
+        </section>
+        <!-- /.bar -->
+        <section class="bar background-white no-mb">
+            <div class="container" data-animate="fadeInUpBig">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="heading text-center">
+                            <h2>Featured Top Sellers</h2>
+                        </div>
+                        <div class="row portfolio">
+                            <div class="col-sm-4">
+                                <div class="box-image">
+                                    <div class="image">
+                                        <img src="img/bacon-bandages.png" alt="" class="img-responsive">
+                                    </div>
+                                    <div class="bg"></div>
+                                    <div class="name">
+                                        <h3><a href="portfolio-detail.html">Bacon Bandages</a></h3>
+                                    </div>
+                                    <div class="text">
+                                        
+                                        <p class="buttons">
+                                            <a href="portfolio-detail.html" class="btn btn-template-transparent-primary">View</a>
+                                            
+                                        </p>
+                                    </div>
+                                </div>
+                                <!-- /.item -->
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="box-image">
+                                    <div class="image">
+                                        <img src="img/pet-sweep.png" alt="" class="img-responsive">
+                                    </div>
+                                    <div class="bg"></div>
+                                    <div class="name">
+                                        <h3><a href="portfolio-detail.html">Pet Sweep</a></h3>
+                                    </div>
+                                    <div class="text">
+                                        
+                                        <p class="buttons">
+                                            <a href="portfolio-detail.html" class="btn btn-template-transparent-primary">View</a>
+                                            
+                                        </p>
+                                    </div>
+                                </div>
+                                <!-- /.item -->
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="box-image">
+                                    <div class="image">
+                                        <img src="img/keyboard-waffle-iron.png" alt="" class="img-responsive">
+                                    </div>
+                                    <div class="bg"></div>
+                                    <div class="name">
+                                        <h3><a href="portfolio-detail.html">Keyboard Waffle Iron</a></h3>
+                                    </div>
+                                    <div class="text">
+                                        
+                                        <p class="buttons">
+                                            <a href="portfolio-detail.html" class="btn btn-template-transparent-primary">View</a>
+                                            
+                                        </p>
+                                    </div>
+                                </div>
+                                <!-- /.item -->
+                            </div>
+                        </div>
+                        <div class="see-more">
+                            <p>Do you want to see more?</p>
+                            <a href="portfolio-4.html" class="btn btn-template-main">View All Listings</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- *** GET IT ***
+_________________________________________________________ -->
+        <div id="get-it">
+            <div class="container">
+                <div class="col-md-8 col-sm-12">
+                    <h3>Need a gag gift or some prank gear?</h3>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                    <a href="#" class="btn btn-template-transparent-primary">This Is The Place</a>
+                </div>
+            </div>
         </div>
-
-    </section>
-
-</div>
