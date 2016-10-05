accbebf89b0935b70efec8e2acc7dde54dd41c55
primalrambler <primal.rambler@gmail.com>
2016-10-05 16:30:03 -0500

    switched to using bootstrap.php instead of individual calls to php files. The individual calls were no longer necessary since they were used for debugging.

--- public/index.php
+++ public/index.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
 
-// require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ . '/../bootstrap.php';
 
-require_once (__DIR__ . '/../controllers/PageController.php');
 
 ?>
 <!DOCTYPE html>
