17dd360279b7515916e7dfd2c28910be50848cc9
primalrambler <primal.rambler@gmail.com>
2016-10-07 09:59:39 -0500

    added functionality to pull username from the add displayed.

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -48,6 +48,10 @@ function pageController($dbc)
             $main_view = __DIR__.'/../views/ads/create.php';
             break;
         case '/ads/show':
+            $adId = Input::get('id');
+            $ad = Ad::find($adId);
+            $sellerId = $ad->user_id;
+            $data['seller'] = User::find($sellerId);
             $data['ad'] = findAdOrRedirect();
             $main_view = __DIR__.'/../views/ads/show.php';
             break;
