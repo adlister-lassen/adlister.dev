8228a4a45e58f188937836202b7492c48c852c66
Rachel Rost <rachel.v.rost@gmail.com>
2016-10-06 19:08:36 -0500

    Updated Styling and UI for 404 page to match the rest of the site

--- views/404.php
+++ views/404.php
@@ -1,9 +1,29 @@
-<div class="container">
+<div id="content">
+            <div class="container">
 
-	<section>
+                <div class="col-sm-6 col-sm-offset-3" id="error-page">
 
-		<h1 class="section-title">404 Error</h1>
+                    <div class="box">
 
-	</section>
+                        <p class="text-center">
+                            <a href="/">
+                                <img src="img/gaglister-logo.png">
+                            </a>
+                        </p>
+                        <br>
 
-</div>
+                        <h3>The Joke Is On You - This Page Has Vanished!</h3>
+                        <br>
+                        <h3 class="text-muted">Error 404 - Page not found</h3>
+						<br>
+                        <p class="buttons"><a href="/" class="btn btn-template-main"><i class="fa fa-home"></i> Go Home</a>
+                        </p>
+                    </div>
+
+
+                </div>
+                <!-- /.col-sm-6 -->
+            </div>
+            <!-- /.container -->
+        </div>
+        <!-- /#content -->
