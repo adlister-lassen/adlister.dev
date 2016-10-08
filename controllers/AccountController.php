7905e9a71502748b9dc2061c949d63b0e9d637ee
primalrambler <primal.rambler@gmail.com>
2016-10-08 14:03:21 -0500

    changed find or redirect function to new function that uses the user's id from the session

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -1,7 +1,5 @@
 <?php
 
-$data['user'] = findLoggedInUserOrRedirect();
-// $ads = getAdsBySeller($dbc);
+$data['user'] = findUserOrRedirect();
 $data['adsToDisplay'] = Ad::findByAdsBySeller($_SESSION['LOGGED_IN_ID']);
-var_dump($data['adsToDisplay']);
 $main_view = __DIR__.'/../views/users/account.php';
