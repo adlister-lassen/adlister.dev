6904edd2b48558747e763a9d83fc3edb4844f02a
primalrambler <primal.rambler@gmail.com>
2016-10-08 13:02:26 -0500

    added method to return all the ads by a user id

--- models/Ad.php
+++ models/Ad.php
@@ -5,7 +5,45 @@ require_once __DIR__ . '/Model.php';
 class Ad extends Model {
 
 	protected static $table = 'ads';
-	
+
+
+    public static function ownsAd($id)
+    {
+        return $this->attributes['user_id'] == $id;
+    }
+
+
+    // finds and returns instance of all ads by a seller
+    public static function findByAdsBySeller($id)
+    {
+
+        self::dbConnect();
+
+        $query = 'SELECT a.id, a.name, a.price, a.date_created, a.ad_views 
+                    FROM ' . self::$table . ' AS a JOIN users AS u ON a.user_id = u.id WHERE u.id = :id';
+
+        $stmt = self::$dbc->prepare($query);
+        $stmt->bindValue(':id', $id, PDO::PARAM_STR);
+        $stmt->execute();
+
+        //Store the resultset in a variable named $result
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // The following code will set the attributes on the calling object based on the result variable's contents
+
+        $instance = null;
+
+        if ( $results )
+        {
+
+            $instance = new static;
+            $instance->attributes = $results;
+        }
+
+        return $instance;
+    }
+
+
 
 }
 
