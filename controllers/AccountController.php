fee47257939895b063528114c166e951dd7e2eb9
primalrambler <primal.rambler@gmail.com>
2016-10-08 13:00:47 -0500

    changing Account to pull a new Ad model function to get ads by seller

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -1,5 +1,7 @@
 <?php
+
 $data['user'] = findLoggedInUserOrRedirect();
-$ads = getAdsBySeller($dbc);
-$data['adsToDisplay'] = $ads;
+// $ads = getAdsBySeller($dbc);
+$data['adsToDisplay'] = Ad::findByAdsBySeller($_SESSION['LOGGED_IN_ID']);
+var_dump($data['adsToDisplay']);
 $main_view = __DIR__.'/../views/users/account.php';
