b457f3f7e0ef117497f628d58ce80d77a292924a
Rachel Rost <rachel.v.rost@gmail.com>
2016-10-09 18:48:54 -0500

    Added favicon to show up in title bar

--- public/js/navbar.js
+++
@@ -1,9 +0,0 @@
-
-    $(function () {
-        $("#home a:contains('/')").li().addClass('active');
-        $("#ads a:contains('ads')").li().addClass('active');
-        $("#login a:contains('login')").li().addClass('active');
-        $("#signup a:contains('signup')").li().addClass('active');
-        $("#create a:contains('create')").li().addClass('active');
-        $("#logout a:contains('logout')").li().addClass('active');
-     });
