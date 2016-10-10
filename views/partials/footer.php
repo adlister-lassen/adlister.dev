99f4fb1accf73e4a2c2eb96905c0735715f10740
Rachel Rost <rachel.v.rost@gmail.com>
2016-10-10 09:26:48 -0500

    Delted unneccesary divs and organized code

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,73 +1,70 @@
 <footer id="footer">
-            <div class="container">
-                <div class="col-md-3 col-sm-6">
-                    <h4>About us</h4>
-                    <p>A place to buy and sell things that make you laugh. Prank Ideas, Gag Gifts and Other Non-Serious Items.</p>
-                    <hr>
-                 
-                    <hr class="hidden-md hidden-lg hidden-sm">
+    <div class="container">
+        <div class="col-md-3 col-sm-6">
+            <h4>About us</h4>
+            <p>A place to buy and sell things that make you laugh. Prank Ideas, Gag Gifts and Other Non-Serious Items.</p>
+            <hr>
+
+            <hr class="hidden-md hidden-lg hidden-sm">
+        </div>
+        <!-- /.col-md-3 -->
+        <div class="col-md-3 col-sm-6">
+
+            <hr class="hidden-md hidden-lg">
+        </div>
+        <!-- /.col-md-3 -->
+        <div class="col-md-3 col-sm-6">
+            <hr class="hidden-md hidden-lg hidden-sm">
+        </div>
+        <!-- /.col-md-3 -->
+        <div class="col-md-3 col-sm-6">
+            <h4>Recently Added Items</h4>
+            <div class="photostream">
+                <div>
+                    <a href="/ads/show?id=18">
+                        <img src="/img/uploads/horn-speaker.png" class="img-responsive" alt="#">
+                    </a>
                 </div>
-                <!-- /.col-md-3 -->
-                <div class="col-md-3 col-sm-6">
-                    
-                    <hr class="hidden-md hidden-lg">
+                <div>
+                    <a href="/ads/show?id=6">
+                        <img src="/img/uploads/bacon-flavored-toothpaste.png" class="img-responsive" alt="#">
+                    </a>
                 </div>
-                <!-- /.col-md-3 -->
-                <div class="col-md-3 col-sm-6">
-                    <hr class="hidden-md hidden-lg hidden-sm">
+                <div>
+                    <a href="/ads/show?id=4">
+                        <img src="/img/uploads/breakfast-maker.png" class="img-responsive" alt="#">
+                    </a>
                 </div>
-                <!-- /.col-md-3 -->
-                <div class="col-md-3 col-sm-6">
-                    <h4>Recently Added Items</h4>
-                    <div class="photostream">
-                        <div>
-                            <a href="/ads/show?id=18">
-                                <img src="/img/uploads/horn-speaker.png" class="img-responsive" alt="#">
-                            </a>
-                        </div>
-                        <div>
-                            <a href="/ads/show?id=6">
-                                <img src="/img/uploads/bacon-flavored-toothpaste.png" class="img-responsive" alt="#">
-                            </a>
-                        </div>
-                        <div>
-                            <a href="/ads/show?id=4">
-                                <img src="/img/uploads/breakfast-maker.png" class="img-responsive" alt="#">
-                            </a>    
-                        </div>
-                        <div>
-                            <a href="/ads/show?id=9">
-                                <img src="/img/uploads/cupcake-floww.png" class="img-responsive" alt="#">
-                            </a>
-                        </div>
-                        <div>
-                            <a href="/ads/show?id=11">
-                                <img src="/img/uploads/disco-ball-helmet.png" class="img-responsive" alt="#">
-                            </a>
-                        </div>
-                        <div>
-                            <a href="/ads/show?id=19">
-                                <img src="/img/uploads/lobster-case.png" class="img-responsive" alt="#">
-                            </a>
-                        </div>
-                    </div>
+                <div>
+                    <a href="/ads/show?id=9">
+                        <img src="/img/uploads/cupcake-floww.png" class="img-responsive" alt="#">
+                    </a>
                 </div>
-                <!-- /.col-md-3 -->
-            </div>
-            <!-- /.container -->
-        <!-- *** COPYRIGHT ***
-_________________________________________________________ -->
-        <div id="copyright">
-            <div class="container">
-                <div class="col-md-12">
-                    <p class="pull-left">&copy; 2016 gaglister</p>
-                    <!-- <p class="pull-right">Template by <a href="http://bootstrapious.com/free-templates">Bootstrapious</a> -->
-                        <!-- Not removing these links is part of the licence conditions of the template. Thanks for understanding :) -->
-                    </p>
+                <div>
+                    <a href="/ads/show?id=11">
+                        <img src="/img/uploads/disco-ball-helmet.png" class="img-responsive" alt="#">
+                    </a>
+                </div>
+                <div>
+                    <a href="/ads/show?id=19">
+                        <img src="/img/uploads/lobster-case.png" class="img-responsive" alt="#">
+                    </a>
                 </div>
             </div>
         </div>
-        <!-- /#copyright -->
-        <!-- *** COPYRIGHT END *** -->
+        <!-- /.col-md-3 -->
     </div>
+    <!-- /.container -->
+    <!-- *** COPYRIGHT ***
+_________________________________________________________ -->
+    <div id="copyright">
+        <div class="container">
+            <div class="col-md-12">
+                <p class="pull-left">&copy; 2016 Gaglister</p>
+            </div>
+        </div>
+    </div>
+    <!-- /#copyright -->
+    <!-- *** COPYRIGHT END *** -->
+
 </footer>
